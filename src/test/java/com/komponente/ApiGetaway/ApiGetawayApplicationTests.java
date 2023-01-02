9e5a0d780d4d807a0c17bc4337c740f467820827
Zarko Radenkovic <zradenkovic520rn@raf.rs>
2023-01-02 13:10:11 +0100

    Initial commit

---
+++ src/test/java/com/komponente/ApiGetaway/ApiGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komponente.ApiGetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
